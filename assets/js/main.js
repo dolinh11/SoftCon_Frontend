f64671118b0c0a4ac6c36dc4a3fca1969263851f
Linh Đỗ Bùi Khánh <dobuikhanhlinh11@gmail.com>
2022-11-04 01:39:02 +0700

    Add search result & Change class name

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
 
 
 /**------------------User Sub------------------ */
-const userSub = document.querySelector('.header__right-user-sub');
-const user = document.querySelector('.header__right-user');
+const userSub = document.querySelector('.header__weblang-sub');
+const user = document.querySelector('.header__weblang-menu');
 user.onclick = function () {
     if (userSub.classList.contains('display')) {
         userSub.classList.remove('display');
     } else {
         userSub.classList.add('display');
+    }
+}
+
+const searchResult = document.querySelector('.header__search-result');
+const searchButton = document.querySelector('.header__search__btn');
+searchButton.onclick = function () {
+    if (searchResult.classList.contains('display')) {
+        searchResult.classList.remove('display');
+    } else {
+        searchResult.classList.add('display');
     }
 }
