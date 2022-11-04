bacf7e647e7cac4a675d0ba096343c688318ad4e
Linh Đỗ Bùi Khánh <dobuikhanhlinh11@gmail.com>
2022-11-04 20:46:17 +0700

    Finish all components

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
+/* Display Web Language */
 
-
-/**------------------User Sub------------------ */
 const userSub = document.querySelector('.header__weblang-sub');
 const user = document.querySelector('.header__weblang-menu');
 user.onclick = function () {
@@ -11,12 +10,34 @@ user.onclick = function () {
     }
 }
 
+/* Exchange Dictionary Language */
+
+function SwapDivsWithClick() {
+    $('#header__dictlang-en, #header__dictlang-vn').first().before(function(){
+    	return $(this).next();
+    });
+}
+
+/* Display Search Result */
+
 const searchResult = document.querySelector('.header__search-result');
-const searchButton = document.querySelector('.header__search__btn');
-searchButton.onclick = function () {
+const searchBtn = document.querySelector('.header__search__btn');
+searchBtn.onclick = function () {
     if (searchResult.classList.contains('display')) {
         searchResult.classList.remove('display');
     } else {
         searchResult.classList.add('display');
+    }
+}
+
+/*Expan Word Meaning*/
+
+const meaning = document.querySelector('.history-item-meaning');
+const expandBtn = document.querySelector('.history-item-expand');
+expandBtn.onclick = function () {
+    if (meaning.classList.contains('display')) {
+        meaning.classList.remove('display');
+    } else {
+        meaning.classList.add('display');
     }
 }
